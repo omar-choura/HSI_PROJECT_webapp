4abe5f77800c5ef40e823a87dbd8d33ae7674d5c
omar-choura <omarchourao@gmail.com>
2024-07-22 00:43:21 +0200

    Responsive

--- src/components/Blockserv.js
+++ src/components/Blockserv.js
@@ -2,76 +2,76 @@ import React from "react";
 import img1 from "../img/blocks/ma1.jpg";
 import img2 from "../img/blocks/ma22.jpg";
 import img3 from "../img/blocks/mach3.png";
-import img4 from "../img/blocks/mach4.png"
+import img4 from "../img/blocks/mach4.png";
+
 const Blockserv = () => {
   return (
-    <>
-      <div className="blocks-services-area text-center mb-100 minus-t-100 wow fadeInUp">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-4">
-              <div className="single-blocks-services">
-                <div className="single-blocks-img">
-                  <img src={img1} alt="img-1" className="img-fluid" />
-                </div>
-                <h3>
-                  <a>Ligne CNC de perçage et débitage des profilés:</a>
-                </h3>
-                <p>
-                  Nombre d'axe et broche de perçage: 3 <br/>
-                  -Type de profilé: HEB, HEA, IPE, UPN... <br/>
-                  -Dimension de profilé: De 160 au 1200mm
-                </p>
+    <div className="blocks-services-area wow fadeInUp">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4 col-sm-6 col-12">
+            <div className="single-blocks-services">
+              <div className="single-blocks-img">
+                <img src={img1} alt="img-1" className="img-fluid" />
               </div>
-            </div>
-
-            <div className="col-md-4">
-              <div className="single-blocks-services">
-                <div className="single-blocks-img">
-                  <img src={img2} alt="img-2" className="img-fluid" />
-                </div>
-                <h3>
-                  <a >Machine CNC Plasma:</a>
-                </h3>
-                <p>
-                -Dimensions maximales <br/>de la tôle : 6 x 2 m<br/>
-                -Epaisseur maximal de la tôle : 65 mm
-                </p>
-                
-              </div>
-            </div>
-
-            <div className="col-md-4">
-              <div className="single-blocks-services">
-                <div className="single-blocks-img">
-                  <img src={img3} alt="img-3" className="img-fluid" />
-                </div>
-                <h3>
-                  <a >Rouleuse de tôles à 4 rouleaux :</a>
-                </h3>
-                <p>
-                - Largeur maximale : 2 m <br/>
-                - Epaisseur maximal : 20mm
-                </p>
-                
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div className="single-blocks-services">
-                <div className="single-blocks-img">
-                  <img src={img4} alt="img-3" className="img-fluid" />
-                </div>
-                <h3>
-                  <a >9 poste de soudure MIG 400A  </a>
-                </h3>
-                
-                
-              </div>
+              <h3>
+                <a>Ligne CNC de perçage et débitage des profilés:</a>
+              </h3>
+              <p>
+                Nombre d'axe et broche de perçage: 3 <br />
+                -Type de profilé: HEB, HEA, IPE, UPN... <br />
+                -Dimension de profilé: De 160 au 1200mm
+              </p>
             </div>
           </div>
+
+          <div className="col-md-4 col-sm-6 col-12">
+            <div className="single-blocks-services">
+              <div className="single-blocks-img">
+                <img src={img2} alt="img-2" className="img-fluid" />
+              </div>
+              <h3>
+                <a>Machine CNC Plasma:</a>
+              </h3>
+              <p>
+                -Dimensions maximales <br />
+                de la tôle : 6 x 2 m
+                <br />
+                -Epaisseur maximal de la tôle : 65 mm
+              </p>
+            </div>
+          </div>
+
+          <div className="col-md-4 col-sm-6 col-12">
+            <div className="single-blocks-services">
+              <div className="single-blocks-img">
+                <img src={img3} alt="img-3" className="img-fluid" />
+              </div>
+              <h3>
+                <a>Rouleuse de tôles à 4 rouleaux :</a>
+              </h3>
+              <p>
+                - Largeur maximale : 2 m <br />
+                - Epaisseur maximal : 20mm
+              </p>
+            </div>
+          </div>
+
+          <div className="col-md-4 col-sm-6 col-12">
+            <div className="single-blocks-services">
+              <div className="single-blocks-img">
+                <img src={img4} alt="img-4" className="img-fluid" />
+              </div>
+              <h3>
+                <a>9 poste de soudure MIG 400A</a>
+              </h3>
+            </div>
+          </div>
+
+          
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/components/Mainslider.js
+++ src/components/Mainslider.js
@@ -1,6 +1,7 @@
 import React from "react";
 import OwlCarousel from "react-owl-carousel";
 import "owl.carousel/dist/assets/owl.carousel.css";
+
 const Mainslider = () => {
   const options = {
     loop: true,
@@ -25,74 +26,67 @@ const Mainslider = () => {
       },
     },
   };
-  return (
-    <>
-      <div className="slider-area">
-        <div className="tractor-main-slider">
-          <OwlCarousel className="owl-carousel owl-theme" {...options}>
-            <div className="tractor-single-slider slid-bg-1">
-              <div className="tractor-single-table">
-                <div className="tractor-single-tablecell">
-                  <div className="container">
-                    <div className="row">
-                      <div className="col-md-12">
-                        <h1>
-                          <span>h </span>steel <span>industry</span> <br />
-                          choix pour l'industrie
-                        </h1>
-                        <p>
-                        La société HASSAIRI STEEL INDUSTRIE opère dans le domaine de construction
-                        métallique. Ses principaux produits consistent en la fabrication d’ouvrages et d’immeubles
-                        en structures métalliques ainsi que des bâtiments industriels métalliques.La société HASSAIRI STEEL INDUSTRIE opère dans le domaine de construction
-                        métallique. Ses principaux produits consistent en la fabrication d’ouvrages et d’immeubles
-                        en structures métalliques ainsi que des bâtiments industriels métalliques.
-                        </p>
-                        <a href="#" className="btn btn-type-1">
-                          Nos services
-                        </a>
-                        <a href="#" className="btn btn-type-2">
-                          Acheter maintenant
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
 
-            <div className="tractor-single-slider slid-bg-2">
-              <div className="tractor-single-table">
-                <div className="tractor-single-tablecell">
-                  <div className="container">
-                    <div className="row">
-                      <div className="col-md-12">
-                        <h1>
+  return (
+    <div className="slider-area">
+      <div className="tractor-main-slider">
+        <OwlCarousel className="owl-carousel owl-theme" {...options}>
+          <div className="tractor-single-slider slid-bg-1">
+            <div className="tractor-single-table">
+              <div className="tractor-single-tablecell">
+                <div className="container">
+                  <div className="row">
+                    <div className="col-md-12">
+                      <h1>
                         <span>h </span>steel <span>industry</span> <br />
-                            choix pour l'industrie
-                        </h1>
-                        <p>
-                        Son expérience, sa bonne réputation, la multitude et la diversité de ces champs d’action,
-                        la compétence de son encadrement et la réussite sans équivoque dans tous les projets
-                        qu’elle a entrepris, a permis à HSI, de devenir un partenaire quasi-incontournable dans la
-                        gestion, le management et la réalisation des plus grands projets internationaux.
-                         
-                        </p>
-                        <a href="#" className="btn btn-type-1">
-                          Nos services
-                        </a>
-                        <a href="#" className="btn btn-type-2">
-                          Acheter maintenant
-                        </a>
-                      </div>
+                        choix pour l'industrie
+                      </h1>
+                      <p>
+                        La société HASSAIRI STEEL INDUSTRIE opère dans le domaine
+                        de construction métallique. Ses principaux produits
+                        consistent en la fabrication d’ouvrages et d’immeubles en
+                        structures métalliques ainsi que des bâtiments
+                        industriels métalliques.
+                      </p>
+                      <a href="#" className="btn btn-type-1">Nos services</a>
+                      <a href="#" className="btn btn-type-2">Acheter maintenant</a>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </OwlCarousel>
-        </div>
+          </div>
+
+          <div className="tractor-single-slider slid-bg-2">
+            <div className="tractor-single-table">
+              <div className="tractor-single-tablecell">
+                <div className="container">
+                  <div className="row">
+                    <div className="col-md-12">
+                      <h1>
+                        <span>h </span>steel <span>industry</span> <br />
+                        choix pour l'industrie
+                      </h1>
+                      <p>
+                        Son expérience, sa bonne réputation, la multitude et la
+                        diversité de ces champs d’action, la compétence de son
+                        encadrement et la réussite sans équivoque dans tous les
+                        projets qu’elle a entrepris, a permis à HSI, de devenir un
+                        partenaire quasi-incontournable dans la gestion, le
+                        management et la réalisation des plus grands projets
+                        internationaux.
+                      </p>
+                      <a href="#" className="btn btn-type-1">Nos services</a>
+                      <a href="#" className="btn btn-type-2">Acheter maintenant</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </OwlCarousel>
       </div>
-    </>
+    </div>
   );
 };
 
